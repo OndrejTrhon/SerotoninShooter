14e0e5821975813950d53427e646eb5badb5bf49
Ondřej Trhoň <ondrej@trhon.cz>
2019-06-02 22:01:44 +0200

    Updated unity version, some new functions rofl

--- SerotoninShooter/Assets/Controller.cs
+++ SerotoninShooter/Assets/Controller.cs
@@ -1,4 +1,4 @@
-﻿    using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,31 +8,43 @@ public class Controller : MonoBehaviour
 {
     public int TickTime;
     private int Level;
+    private int Time;
 
     public Text LevelText;
-    
-    public GameObject SoundPlayer; 
-    public GameObject SoundPlayer2;
-    public GameObject SoundPlayer3;
-    public GameObject SoundPlayer4;
-    public GameObject SoundChecker;
-
-    
-
+    public Text TimeText;
     // Start is called before the first frame update
     void Start()
     {
         Level = 10;
+        Time = 0;
         InvokeRepeating("Ticker", 1.0f, TickTime);
+        InvokeRepeating("Clock", 1.0f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-               
+     if (Input.GetKeyDown(KeyCode.E)) {
+              Level = Level + 3;
+              LevelText.text = "Level: " + Level.ToString ();
+
+     }
+          
     }
 
     //Main ticking function
+
+    void Clock() {
+        Debug.Log("Time"); 
+        if (Time <= 23) {
+            Time = Time + 1;
+        } else {
+            Time = 0;
+        }
+        Debug.Log(Time);
+        TimeText.text = "Time: " + Time.ToString ();
+    }
+
     void Ticker()
     {
               Debug.Log("Test"); 
@@ -44,8 +56,26 @@ public class Controller : MonoBehaviour
     void LevelObserver() {
         Level--;
         LevelText.text = "Level: " + Level.ToString ();
-
-
     }
 
+    //UpdateDifficulty according to the level
+    void UpdateDifficulty() {
+        Debug.Log("UpdateDifficulty"); 
+    }
+
+    void TakeMedication() {
+        Level = Level + 3;
+        LevelText.text = "Level: " + Level.ToString ();
+        CheckTime();
+    }
+
+    void CheckTime() {
+
+        SetState();
+    }
+
+    void SetState() {
+        Debug.Log("State"); 
+
+    }
 }
