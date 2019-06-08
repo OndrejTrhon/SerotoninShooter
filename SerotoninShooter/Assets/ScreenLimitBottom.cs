0a30fe1e4dab64b7208377a983724e0c6bdb062f
Ondřej Trhoň <ondrej@trhon.cz>
2019-06-08 20:15:13 +0200

    New Boundaries for enemies / enemy missed count

---
+++ SerotoninShooter/Assets/ScreenLimitBottom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenLimitBottom : MonoBehaviour
+{
+
+    public Text EnemiesLeftText;
+    private int EnemiesLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemiesLeft = 0;
+    }  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D other) //the code below is only called when an object that includes a collider enters the trigger 2D collider attached to the screen limit object.
+{
+    EnemiesLeft = EnemiesLeft +1;
+    Destroy (other.gameObject); //Destroy the object that passes through the screen limit collider.
+    EnemiesLeftText.text = "Serotonins missed: " + EnemiesLeft.ToString ();
+
+
+}
+}
---
+++ SerotoninShooter/Assets/ScreenLimitTop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenLimitTop : MonoBehaviour
+{
+
+    
+        // Just destroys passing objects
+
+    void Start()
+    {
+    }  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D other) //the code below is only called when an object that includes a collider enters the trigger 2D collider attached to the screen limit object.
+{
+    Destroy (other.gameObject); //Destroy the object that passes through the screen limit collider.
+    
+}
+}
